75b318eddb06b1b858e415f2084fcd55c3863baf
Yuri Gravata <ygravata@gmail.com>
2020-04-25 16:42:54 -0300

    doing now the style part

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -5,6 +5,6 @@ class Dose < ApplicationRecord
   validates :description, presence: true
   validates :cocktail, presence: true
   validates :ingredient, presence: true
-  validates :cocktail,uniqueness: { scope: :ingredient }
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
 
