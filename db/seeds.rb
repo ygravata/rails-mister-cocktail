fac6ec8bf68b3fb4c834cf93bf67b3661a82fb4b
Yuri Gravata <ygravata@gmail.com>
2020-04-25 13:14:16 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+drinks = user['drinks']
+
+drinks.each do |drink|
+  Ingredient.create!(name: drink['strIngredient1'])
+end
