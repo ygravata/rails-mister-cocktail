5143d6bb532c21aa5bc3fb0949eb962da6b58e65
Yuri Gravata <ygravata@gmail.com>
2020-04-28 21:12:38 -0300

    trying to add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: %i[index show new create destroy] do
+  resources :cocktails, only: %i[root index show new create destroy edit update] do
     resources :doses, only: %i[new create]
   end
   resources :doses, only: %i[destroy]
